ed421b3dc7aefb0e854c0be0b444c603eef5daf7
Blerargh <49093336+Blerargh@users.noreply.github.com>
2025-06-16 21:05:46 +0800

    scrape title test

--- ecolens-extension/src/components/header/Header.tsx
+++ ecolens-extension/src/components/header/Header.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../styles/header.css'
 
 const Header: React.FC = () => {
+  const [ productTitle, setProductTitle ] = useState<string>('');
+  const handleClick = async () => {
+    const [ tab ] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    if (!tab.id) {
+      console.error('Active tab has no id');
+      return;
+    }
+
+    const results = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ["src/scripts/siteinfo.js"]
+    });
+
+    console.log(results);
+
+    // Assuming the script returns a string as its result
+    const title = results && results[0] && typeof results[0].result === 'string' ? results[0].result : '';
+    setProductTitle(title);
+  }
+
   return (
-    <div className='header-container'>Header</div>
+    <>
+      <div className='header-container'>
+        <div className='product-details'>
+          Product Name: {productTitle}
+        </div>
+        <button onClick={handleClick}>Refresh</button>
+      </div>
+    </>
   )
 }
 
---
+++ ecolens-extension/src/scripts/siteinfo.js
@@ -0,0 +1,6 @@
+
+(function () {
+  const title = document.querySelector('#productTitle').innerHTML;
+  console.log(title);
+  return title; 
+})();
