f24798c9e9685875369baa22dc211e1bcc9c7b28
Blerargh <49093336+Blerargh@users.noreply.github.com>
2025-06-17 16:59:40 +0800

    feat: add DetailsPopup component with motion effects and styles; update dependencies in package.json and package-lock.json

--- ecolens-extension/src/components/footer/DetailsPopup.tsx
+++ ecolens-extension/src/components/footer/DetailsPopup.tsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { motion } from "motion/react"
+import { IoMdCloseCircle } from "react-icons/io";
+import '../../styles/detailspopup.css'
 
-const DetailsPopup: React.FC = () => {
+import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
+interface SustainabilityInfo {
+  criteria: string;
+  value: number;
+  progressBar: number; // Out of 100
+  color: string; // Hex info
+}
+
+interface Props {
+  info: SustainabilityInfo[] | [];
+  description: string | 'Unable to Retrieve Information';
+  setShowPopup: (showPopup: boolean) => void; 
+}
+
+const DetailsPopup: React.FC<Props> = (props) => {
+  const { info, description, setShowPopup } = props
   return (
-    <div>DetailsPopup</div>
+    <motion.div 
+      className='details-popup-container'
+      initial={{ y: 500 }}
+      animate={{ y: 0, transition: { duration: 0.4 } }}
+    >
+      <div className='show-more-header'>
+        <h3>Detailed Analysis</h3>
+        <IoMdCloseCircle className='close-button' onClick={() => setShowPopup(false)}/>
+      </div>
+      <div className='show-more-info-container'>
+        <div className='popup-bar-items-container'>
+          {info.map((displayInfo) => (
+            <div className='bar-item'>
+              <CircularProgressbar
+                className='progress-bar'
+                value={displayInfo.progressBar}
+                text={`${displayInfo.value}`}
+                styles={buildStyles({
+                  pathColor: displayInfo.color,
+                  textColor: '#fff',
+                })}
+              />
+              <p className='bar-item-text'>{displayInfo.criteria}</p>
+            </div>
+          ))}
+        </div>
+        <div className='popup-description'>
+          <h3>Explanation</h3>
+          {description}
+        </div>
+      </div>
+    </motion.div>
   )
 }
 
--- ecolens-extension/src/components/footer/Footer.tsx
+++ ecolens-extension/src/components/footer/Footer.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../styles/footer.css'
 import { buildStyles, CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import DetailsPopup from './DetailsPopup';
 
 interface SustainabilityInfo {
   criteria: string;
@@ -15,13 +16,44 @@ interface FooterProps {
 }
 
 const Footer: React.FC<FooterProps> = (props) => {
+
+  const [showMore, setShowMore] = useState<boolean>(false)
+  const handleShowMoreClick = () => {
+    setShowMore(true);
+  }
+
+
   let { info } = props
 
-  //test
+  // for testing (to be removed)
   info = [
     {
       criteria: 'test1',
       value: 100,
+      progressBar: 90, // Out of 100
+      color: '#fff' // Hex info
+    },
+    {
+      criteria: 'test2',
+      value: 10023,
+      progressBar: 90, // Out of 100
+      color: '#fff' // Hex info
+    },
+    {
+      criteria: 'test2',
+      value: 10023,
+      progressBar: 90, // Out of 100
+      color: '#fff' // Hex info
+    },
+    {
+      criteria: 'test2',
+      value: 10023,
+      progressBar: 90, // Out of 100
+      color: '#fff' // Hex info
+    },
+    {
+      criteria: 'test2',
+      value: 10023,
       progressBar: 90, // Out of 100
       color: '#fff' // Hex info
     },
@@ -35,25 +67,28 @@ const Footer: React.FC<FooterProps> = (props) => {
   const previewDisplay = info.length > 2 ? info.slice(0, 2) : info;
 
   return (
-    <div className='footer-container'>
-      <div className='bar-items-container'>
-        {previewDisplay.map((displayInfo) => (
-          <div className='bar-item'>
-            <CircularProgressbar
-              className='progress-bar'
-              value={displayInfo.progressBar}
-              text={`${displayInfo.value}`}
-              styles={buildStyles({
-                pathColor: displayInfo.color,
-                textColor: '#fff',
-              })}
-            />
-            <p className='bar-item-text'>{displayInfo.criteria}</p>
-          </div>
-        ))}
+    <>
+      {showMore ? <DetailsPopup info={info} description={'lorem ipsum'} setShowPopup={setShowMore}/> : <></>}
+      <div className='footer-container'>
+        <div className='bar-items-container'>
+          {previewDisplay.map((displayInfo) => (
+            <div className='bar-item'>
+              <CircularProgressbar
+                className='progress-bar'
+                value={displayInfo.progressBar}
+                text={`${displayInfo.value}`}
+                styles={buildStyles({
+                  pathColor: displayInfo.color,
+                  textColor: '#fff',
+                })}
+              />
+              <p className='bar-item-text'>{displayInfo.criteria}</p>
+            </div>
+          ))}
+        </div>
+        <button className='show-more-button' onClick={handleShowMoreClick}>Show more →</button>
       </div>
-      <button className='show-more-button'>Show more →</button>
-    </div>
+    </>
   )
 }
 
